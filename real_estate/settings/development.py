cf84ba3a59840d3cc2270873ac22f2df4177c8d2
buda <vbm7752@gmail.com>
2024-06-16 01:48:43 +0300

    new files

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -15,23 +15,23 @@ SITE_NAME = "Rentify Estate"
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': env('DATABASE_ENGINE'),
-        'NAME': os.path.join(BASE_DIR, env('DATABASE_NAME')),
-    }
-}
-
-
-# Uncomment this section if using PostgreSQL
 # DATABASES = {
 #     'default': {
 #         'ENGINE': env('DATABASE_ENGINE'),
-#         'NAME': env('DATABASE_NAME'),
-#         'USER': env('DATABASE_USER'),
-#         'PASSWORD': env('DATABASE_PASSWORD'),
-#         'HOST': env('DATABASE_HOST'),
-#         'PORT': env('DATABASE_PORT', default='5432'),
+#         'NAME': os.path.join(BASE_DIR, env('DATABASE_NAME')),
 #     }
 # }
+
+
+#Uncomment this section if using PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': env('DATABASE_ENGINE'),
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT', default='5432'),
+    }
+}
 
